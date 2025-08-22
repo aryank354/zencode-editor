d72f58384bed50456b901b46df3557478a260f08
aryankanojia354 <aryankanojia354@gmail.com>
2025-08-22 06:44:02 +0000

    files uploaded

---
+++ lib/template.ts
@@ -0,0 +1,10 @@
+// templatePaths.ts
+export const templatePaths = {
+    REACT: '/zencode-starters/react-ts',
+    NEXTJS: '/zencode-starters/nextjs-new',
+    EXPRESS: '/zencode-starters/express-simple',
+    VUE: '/zencode-starters/vue',
+    HONO: '/zencode-starters/hono-nodejs-starter',
+    ANGULAR: '/zencode-starters/angular',
+  };
+  
