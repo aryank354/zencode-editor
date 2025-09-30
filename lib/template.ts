bd8e584bea70aaa1468ce2dfc33a2b185a72f50d
aryank354 <aryankanojia354@gmail.com>
2025-09-30 05:38:36 +0000

    temp created

--- lib/template.ts
+++ lib/template.ts
@@ -1,9 +1,9 @@
-// templatePaths.ts
+// lib/template.ts
 export const templatePaths = {
-  REACT: 'zencode-starters/react-ts',
-  NEXTJS: 'zencode-starters/nextjs-new',
-  EXPRESS: 'zencode-starters/express-simple',
-  VUE: 'zencode-starters/vue',
-  HONO: 'zencode-starters/hono-nodejs-starter',
-  ANGULAR: 'zencode-starters/angular',
+  REACT: "public/zencode-starters/react-ts",
+  NEXTJS: "public/zencode-starters/nextjs-new",
+  EXPRESS: "public/zencode-starters/express-simple",
+  VUE: "public/zencode-starters/vue",
+  HONO: "public/zencode-starters/hono-nodejs-starter",
+  ANGULAR: "public/zencode-starters/angular",
 };
