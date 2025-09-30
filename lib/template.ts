52cb1c95c0580a69a55f099507c0253f0fc8e4d3
aryank354 <aryankanojia354@gmail.com>
2025-09-30 05:03:36 +0000

    path changed

--- lib/template.ts
+++ lib/template.ts
@@ -1,10 +1,9 @@
 // templatePaths.ts
 export const templatePaths = {
-    REACT: '/zencode-starters/react-ts',
-    NEXTJS: '/zencode-starters/nextjs-new',
-    EXPRESS: '/zencode-starters/express-simple',
-    VUE: '/zencode-starters/vue',
-    HONO: '/zencode-starters/hono-nodejs-starter',
-    ANGULAR: '/zencode-starters/angular',
-  };
-  
+  REACT: 'zencode-starters/react-ts',
+  NEXTJS: 'zencode-starters/nextjs-new',
+  EXPRESS: 'zencode-starters/express-simple',
+  VUE: 'zencode-starters/vue',
+  HONO: 'zencode-starters/hono-nodejs-starter',
+  ANGULAR: 'zencode-starters/angular',
+};
